27c55a98b97fe6e6f35d502c0f0daa4d16a9bef0
PrawiraRzkN <prawirarzkn94@gmail.com>
2023-08-30 23:05:24 +0700

    upload repo

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z8l-2abf4q006mk*fr)sdjtj7l(t5i=*$!o&6iaz^g6&ai4b71'
+SECRET_KEY = 'django-insecure-vgzv9dmqxiaiw%rmv^-)^1jv_c#b0npcaj(_=#i6()izv=!#$^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
